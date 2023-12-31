a7eaba97389ae3c152b82ee23b660e17e4c73cf6
Dan Komnick <daniel.komnick@gmail.com>
2023-12-31 17:43:16 -0500

    completed - part 1

---
+++ 020/snake_game/main.py
@@ -0,0 +1,28 @@
+from turtle import Screen, Turtle
+from snake import Snake
+import time
+
+snake = Snake()
+
+screen = Screen()
+screen.setup(width=600,height=600)
+screen.bgcolor("black")
+screen.title('Snake Game')
+screen.tracer(0)
+screen.listen()
+screen.onkey(snake.up, "Up")
+screen.onkey(snake.down, "Down")
+screen.onkey(snake.left, "Left")
+screen.onkey(snake.right, "Right")
+
+
+
+game_running = True
+while game_running:
+    screen.update()
+    time.sleep(0.1)
+    snake.move()
+
+
+
+
